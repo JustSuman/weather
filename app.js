6cf109c6da20c6db2311359ec635c683285e2757
Suman Shrestha <118437501+JustSuman@users.noreply.github.com>
2023-04-01 13:04:22 +0545

    Add files via upload

--- app.js
+++ app.js
@@ -21,13 +21,22 @@ async function weather(city) {
     }
   }
 function display(data) {
+    //background image according to the location 
     document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + data.name + "')";
+    //tempreture
     document.querySelector(".temperature").innerHTML = `${data.main.temp} °C`;
+    //country name
     document.querySelector(".city").innerHTML = `${data.name}, ${data.sys.country}`;
+    //weather icon
     document.querySelector(".weather img").src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    //weather description
     document.querySelector(".description").innerHTML = data.weather[0].description;
+    //wind
     document.querySelector(".wind").innerHTML = `WIND: ${data.wind.speed} km/hr`;
+    //humidity
     document.querySelector(".humidity").innerHTML = `HUMIDITY: ${data.main.humidity}%`;
+    //loading message not to be displayed when add are loaded
+    document.querySelector("#loading-message").style.display = "none";
 }
 
 document.addEventListener("keydown", function (event) {
